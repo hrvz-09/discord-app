941da6204ef0c5453dcdaa33d4b9f6472de67274
Jacob Harvey <91164944+hrvz-09@users.noreply.github.com>
2025-06-26 19:39:20 -0500

    Signup function created and tested via Postman

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,61 @@
-export const signup = (req, res) => {
-    res.send("signup route");
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    // Make sure all fields are filled out
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are requried" });
+    }
+    // Verify password length during creation
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Passoword mst be at least" + " 6 characters" });
+    }
+
+    // Verify email is not already in use
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists." });
+    }
+
+    // hash password using bcrypt package
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create the user with the verified/hashed credentials
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      // generate jwt token
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user credentials" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
+
 export const login = (req, res) => {
-    res.send("login route");
+  res.send("login route");
 };
+
 export const logout = (req, res) => {
-    res.send("logout route");
+  res.send("logout route");
 };

c3c4cccd82b42aa5932f1e0139f6e057c65d81d3
Jacob Harvey <91164944+hrvz-09@users.noreply.github.com>
2025-06-30 10:36:09 -0500

    Fixed userId bug

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  checkAuth,
   login,
   logout,
   signup,
@@ -13,5 +14,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.put("update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
